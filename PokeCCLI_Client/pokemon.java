8fba678a3573b3d7aa0f4feb3c48cb7cf127b448
Nicholas Goosen <nicholas.goosen@gmail.com>
2016-04-03 16:23:34 -0400

    Added files via upload

---
+++ PokeCCLI_Client/pokemon.java
@@ -0,0 +1,83 @@
+package PokeCLI_Client;
+
+public class pokemon {
+	public int[] ev = {0, 0, 0, 0, 0 ,0}; //hp atk def spa spd spe
+	public static final int MAX_EV = 510;
+	
+	public String name = "empty";
+	
+	public pokemon(String arg){
+		name = arg;
+	}
+	
+	private boolean validEv(int arg, int index){
+		if ((arg > 255) || (arg < 0)){
+			return false;
+		}else{
+			switch(index){
+			case 0:
+				return ((arg + ev[1] + ev[2] + ev[3] + ev[4] + ev[5]) <= MAX_EV);
+			case 1:
+				return ((ev[0] + arg + ev[2] + ev[3] + ev[4] + ev[5]) <= MAX_EV);
+			case 2:
+				return ((ev[0] + ev[1] + arg + ev[3] + ev[4] + ev[5]) <= MAX_EV);
+			case 3:
+				return ((ev[0] + ev[1] + ev[2] + arg + ev[4] + ev[5]) <= MAX_EV);
+			case 4:
+				return ((ev[0] + ev[1] + ev[2] + ev[3] + arg + ev[5]) <= MAX_EV);
+			case 5:
+				return ((ev[0] + ev[1] + ev[2] + ev[3] + ev[4] + arg) <= MAX_EV);
+			}
+		return false;
+		}
+	}
+	
+	public void setHp(int arg){
+		if(validEv(arg, 0)){
+			ev[0] = arg;
+		}else{
+			System.out.println("Error: ev out of range. \n" + 
+					"Ev's must be between 0 and 255, total Evs must be less than or equal to 510.");
+		}
+	}
+	public void setAtk(int arg){
+		if(validEv(arg, 1)){
+			ev[1] = arg;
+		}else{
+			System.out.println("Error: ev out of range. \n" + 
+					"Ev's must be between 0 and 255, total Evs must be less than or equal to 510.");
+		}
+	}
+	public void setDef(int arg){
+		if(validEv(arg, 2)){
+			ev[2] = arg;
+		}else{
+			System.out.println("Error: ev out of range. \n" + 
+					"Ev's must be between 0 and 255, total Evs must be less than or equal to 510.");
+		}
+	}
+	public void setSpa(int arg){
+		if(validEv(arg, 3)){
+			ev[3] = arg;
+		}else{
+			System.out.println("Error: ev out of range. \n" + 
+					"Ev's must be between 0 and 255, total Evs must be less than or equal to 510.");
+		}
+	}
+	public void setSpd(int arg){
+		if(validEv(arg, 4)){
+			ev[4] = arg;
+		}else{
+			System.out.println("Error: ev out of range. \n" + 
+					"Ev's must be between 0 and 255, total Evs must be less than or equal to 510.");
+		}
+	}
+	public void setSpe(int arg){
+		if(validEv(arg, 5)){
+			ev[5] = arg;
+		}else{
+			System.out.println("Error: ev out of range. \n" + 
+					"Ev's must be between 0 and 255, total Evs must be less than or equal to 510.");
+		}
+	}
+}
---
+++ PokeCCLI_Client/team.java
@@ -0,0 +1,19 @@
+package PokeCLI_Client;
+
+public class team {
+	public String name = "";
+	
+	public static pokemon[] pokemonList = new pokemon[6];
+	
+	public team(String arg){
+		name = arg;
+	}
+	
+	public void setPokemon(int arg1, String arg2){
+		if(pokeConst.isName(arg2)&& arg1 >= 0 && arg1 <= 5){
+			pokemonList[arg1] = new pokemon(arg2);
+		}else{
+			System.out.println("Error: invalid pokemon index / name.");
+		}
+	}
+}
